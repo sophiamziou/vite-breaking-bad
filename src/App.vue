158229a08302c68126ae419796f09704f4920b97
sophiamziou <sofiamziou@gmail.com>
2023-01-16 18:56:04 +0100

    header done

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  export default {
+      components:{
+          AppHeader
+      }
+  }
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<style lang="scss">
+        @use './styles/generals.scss' as *;
+</style>
